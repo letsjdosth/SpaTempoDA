a34153af7deef5763ebf1e1411ca4d2c23f2be4a
Seokjun Choi <Choi.Seokjun@outlook.com>
2020-04-09 02:37:12 +0900

    HW1 prob1 add traceplot, prob2 refactoring and saving plots

--- HW1/HW1sol_problem1.R
+++ HW1/HW1sol_problem1.R
@@ -6,7 +6,6 @@
 # vscode run as source : ctrl+shift+s
 # vscode run selected block only : ctrl+enter
 
-
 # 1. Suppose we want to simulate a random vector Y ∼ N(µ,Σ). If Σ (Matern) is symmetric and positive definite, 
 # it can be represented using the Cholesky decomposition Σ = LL', where L is a lower triangular matrix. 
 # Consider the following algorithm for simulating Y : 
@@ -19,7 +18,10 @@
 # You may use the built-in function chol for the Cholesky decomposition and rnorm to generate Z.
 # (c) For a mean and covariance function of your choosing, 
 # use your code from (b) and make a few plots illustrating realizations of a Gaussian process on [0;1], 
-# but changing the diﬀerent parameters in the model. These diﬀerences will be easier to see if you keep the same Z sample but just change mu and Sigma.
+# but changing the different parameters in the model. These differences will be easier to see if you keep the same Z sample but just change mu and Sigma.
+
+library(fields)
+set.seed(2019311252)
 
 
 rnorm_chol_algorithm = function(mu, Sigma) {
@@ -41,20 +43,49 @@ test_suite_for_2d = function(mu_2dim, cov_2by2dim, sim_num=10000){
     }
     par(mfrow=c(3,1))
     plot(test_result_x1, test_result_x2, main='scatterplot', xlab='1st dim', ylab='2nd dim')
-    plot(seq(0,1,length.out=sim_num), test_result_x1, type='l', xlab='iteration(normalized)', ylab='value')
-    plot(seq(0,1,length.out=sim_num), test_result_x2, type='l', xlab='iteration(normalized)', ylab='value')
+    plot(seq(0,1,length.out=sim_num), test_result_x1, type='l', xlab='iteration(normalized)', ylab='value', main='1st dim')
+    plot(seq(0,1,length.out=sim_num), test_result_x2, type='l', xlab='iteration(normalized)', ylab='value', main='2nd dim')
 }
+
+test_suite_for_traceplot = function(mu_2dim, cov_2by2dim, process_num=5, sim_num=100){
+    result_x1 = matrix(0, process_num, sim_num)
+    result_x2 = matrix(0, process_num, sim_num)
+
+    for(i in 1:process_num){
+        for(j in 1:sim_num){
+            iter_sample = rnorm_chol_algorithm(mu_2dim, cov_2by2dim)
+            result_x1[i,j] = iter_sample[1]
+            result_x2[i,j] = iter_sample[2]
+        }
+    }
+    par(mfrow=c(2,1))
+    grid = seq(0,1,length.out=sim_num)
+    plot(grid, result_x1[1,], type='l', xlab='iteration(normalized)', ylab='value', main='1st dim')
+    for(i in 2:process_num){
+        lines(grid, result_x1[i,])
+    }
+    plot(grid, result_x2[1,], type='l', xlab='iteration(normalized)', ylab='value', main='2nd dim')
+    for(i in 2:process_num){
+        lines(grid, result_x2[i,])
+    }
+}
+
 
 # test 1 : ordinary gaussian
 test1_mu = c(1, -1)
 test1_Sigma = matrix(c(1,-1,-1,2),2,2)
-test_suite_for_2d(test1_mu, test1_Sigma)
 
+
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test1_scatter.png")
+test_suite_for_2d(test1_mu, test1_Sigma)
+dev.off()
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test1_traceplot.png")
+test_suite_for_traceplot(test1_mu, test1_Sigma)
+dev.off()
 
 # for test 2,3,4, I use 
 # fields$Matern
 # if you don't have 'fields' packages, install first and run below.
-library(fields)
 
 
 # test 2 : with Matern covariance (= exp cov)
@@ -67,34 +98,40 @@ test2_Sigma = Matern(test2_distance_mat, test2_matern_param_range, test2_matern_
 # nu = smoothness in lec.note, range=rho?? in lec.note (maybe!)
 print("set covariance matrix of test 2")
 print(test2_Sigma)
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test2_scatter.png")
 test_suite_for_2d(test2_mu, test2_Sigma)
-
+dev.off()
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test2_traceplot.png")
+test_suite_for_traceplot(test2_mu, test2_Sigma)
+dev.off()
 
 
 # test 3 : with Matern covariance (small nu)
 test3_mu = c(1, -1)
-
-# library(fields)
-# fields$Matern
 test3_matern_param_nu = 0.1
 test3_matern_param_range = 1
 test3_distance_mat = matrix(c(0,0.2,0.2,0),2,2)
 test3_Sigma = Matern(test3_distance_mat, test3_matern_param_range, test3_matern_param_nu)
 print("set covariance matrix of test 3")
 print(test3_Sigma)
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test3_scatter.png")
 test_suite_for_2d(test3_mu, test3_Sigma)
-
+dev.off()
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test3_traceplot.png")
+test_suite_for_traceplot(test3_mu, test3_Sigma)
+dev.off()
 
 # test 4 : with Matern covariance (big nu)
 test4_mu = c(1, -1)
-
-# library(fields)
-# fields$Matern
 test4_matern_param_nu = 1
 test4_matern_param_range = 1
 test4_distance_mat = matrix(c(0,0.2,0.2,0),2,2)
 test4_Sigma = Matern(test4_distance_mat, test4_matern_param_range, test4_matern_param_nu)
 print("set covariance matrix of test 4")
 print(test4_Sigma)
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test4_scatter.png")
 test_suite_for_2d(test4_mu, test4_Sigma)
-
+dev.off()
+png(filename="C:/gitProject/SpaTempoDA/HW1/prob1_test4_traceplot.png")
+test_suite_for_traceplot(test4_mu, test4_Sigma)
+dev.off()
--- HW1/HW1sol_problem2.R
+++ HW1/HW1sol_problem2.R
@@ -1,3 +1,10 @@
+# R code created by Choi Seokjun
+# UTF-8 CRLF
+# windows 10 64-bit.
+# R 3.5.2
+
+# Note: much part of this code is reconstructed from Park's code.
+
 # 2. The file CAtemps.RData contains two R objects of class SpatialPointsDataFrame, called CAtemp and CAgrid. 
 # CAtemp contains average temperatures from 1961-1990 at 200 locations (latitude and longitude) in California in degrees Fahrenheit, 
 # along with their elevations in meters. CAgrid contains elevations in meters over a grid of locations. 
@@ -19,6 +26,7 @@ library(fields)
 library(classInt)
 library(maps)
 
+## function for plotting
 ploteqc = function(spobj, z, breaks, ...){
   pal = tim.colors(length(breaks)-1)
   fb = classIntervals(z, n = length(pal), 
@@ -28,75 +36,91 @@ ploteqc = function(spobj, z, breaks, ...){
   image.plot(legend.only = TRUE, zlim = range(breaks), col = pal)
 }
 
+
+## Data loading and EDA
+
 load("C:/gitProject/SpaTempoDA/HW1/CAtemps.RData")
 # head(CAtemp)
 # typeof(CAtemp) #S4
-# names(CAtemp) #[1] "avgtemp"   "elevation"
-## lat, lon, avgtemp, elevation
+# names(CAtemp) #[1] "avgtemp"   "elevation" ## lat, lon, avgtemp, elevation
 
-## Plotting
+# Plotting for EDA
 # range(CAtemp$avgtemp)
-breaks = 30:75
+breaks = 30:75 #<-see range and set by your hand!
 x11()
 ploteqc(CAtemp, CAtemp$avgtemp, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
 title(main = "Average Annual Temperatures, 1961-1990, Degrees F")
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAtemp_avgtemp.png")
+dev.off()
 
 
 
-# (a) Using the CAtemp data, form a preliminary estimate of β using ordinary least squares and make a color plot of the residuals. 
+## (a) Using the CAtemp data, form a preliminary estimate of β using ordinary least squares and make a color plot of the residuals. 
 # Include your estimates and plot.
 
 ols = lm(avgtemp~lon+lat+elevation, data=CAtemp)
-summary(ols)
+print("ols coefficients")
+print(ols$coeff) #estimates
+print('----------------')
 # x11()
-# plot(ols$residual)
-abline(0,0)
+# plot(ols$residual) # r ordinary plot
+# abline(0,0)
 
 CAtemp$ols.residual = ols$residual
 
-## Plotting
+# Plotting
 # range(CAtemp$ols.residual)
 breaks = seq(-7, 7, by = 0.7)
 x11()
 ploteqc(CAtemp, CAtemp$ols.residual, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
 title(main = "ols residual")
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAtemp_ols_residual.png")
+dev.off()
 
 
 
-
-# (b) Estimate the variogram nonparametrically and then fit the exponential variogram to it using weighted least squares. 
+## (b) Estimate the variogram nonparametrically and then fit the exponential variogram to it using weighted least squares. 
 # Make and include a plot of the nonparametric and parametric variogram functions. 
 # Also store your parameter estimates and report them.
 
-
-##  Nonparametric estimation of the variogram
-
+#  Nonparametric estimation of the variogram
 vg = variogram(ols.residual ~ 1, data = CAtemp, width=50) # What happens when we change the bin width?
-x11()
-plot(vg, xlab = "Distance", ylab = "Semi-variogram estimate", width=5) 
-
+print(plot(vg, xlab = "Distance", ylab = "Semi-variogram estimate"))
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAtemp_variogram.png")
+dev.off()
 
 vgangle = variogram(ols.residual ~ 1, data = CAtemp, alpha = c(0, 45, 90, 135))
 x11()
-plot(vgangle, xlab = "Distance", ylab = "Semi-variogram estimate")
+plot.new()
+print(plot(vgangle, xlab = "Distance", ylab = "Semi-variogram estimate"))
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAtemp_variogram_angle.png")
+dev.off()
 
-##  Modeling the semivariogram
+#  Modeling the semivariogram
+x11()
+plot.new()
+print(show.vgms())
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAtemp_variogram_various_fit.png")
+dev.off()
 
-# show.vgms()
 
-# second argument has starting values
-fitvg = fit.variogram(vg, vgm(1, "Exp", range=300, nugget=3))
+fitvg = fit.variogram(vg, vgm(1, "Exp", range=300, nugget=3)) # second argument has starting values
 # print(fitvg)
+# estimated parameters of variance term
 s2.hat = fitvg$psill[2]
 rho.hat = fitvg$range[2]
 tau2.hat = fitvg$psill[1]
 x11()
-plot(vg, fitvg, xlab = "Distance", ylab = "Semi-variogram estimate")
+plot.new()
+print(plot(vg, fitvg, xlab = "Distance", ylab = "Semi-variogram estimate"))
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAtemp_variogram_exp_fit.png")
+dev.off()
 
 
-# (c) We will now form the GLS estimate of β by hand, rather than using the gls function. 
+
+## (c) We will now form the GLS estimate of β by hand, rather than using the gls function. 
 # (This function doesn’t handle longitude and latitude well, and I also want to give you some practice with matrix calculations in R.) 
 # - Use the rdist function in fields to create a matrix of distances (in miles) between pairs of locations in CAtemp. 
 # - Create the covariance matrix, plugging in your estimates from the fitted variogram. 
@@ -108,105 +132,106 @@ plot(vg, fitvg, xlab = "Distance", ylab = "Semi-variogram estimate")
 # lec1.2-40page
 # lec2.1-24page
 
-mean_earth_radius_in_miles = 3958.8 #by google
-chordaldist_x = mean_earth_radius_in_miles * cos(CAtemp$lat) * cos(CAtemp$lon)
-chordaldist_y = mean_earth_radius_in_miles * cos(CAtemp$lat) * sin(CAtemp$lon)
-chordaldist_z = mean_earth_radius_in_miles * sin(CAtemp$lat)
-chordaldist_mat = cbind(chordaldist_x, chordaldist_y, chordaldist_z)
+make_chordaldist_u1u2_for_rdist = function(lat, lon){
+  mean_earth_radius_in_miles = 3958.8 #by google
+  chordaldist_x = mean_earth_radius_in_miles * cos(lat) * cos(lon)
+  chordaldist_y = mean_earth_radius_in_miles * cos(lat) * sin(lon)
+  chordaldist_z = mean_earth_radius_in_miles * sin(lat)
+  chordaldist_mat = cbind(chordaldist_x, chordaldist_y, chordaldist_z)
+  return(chordaldist_mat)
+}
 
-dist_mat = rdist(chordaldist_mat, chordaldist_mat)
-# dim(dist_mat)
+make_EXPcov_mat_without_nugget = function(dist_mat, rho, sigma_sqaure){
+  #lecture note 1-2. page 40
+  num_row = dim(dist_mat)[1] 
+  num_col = dim(dist_mat)[2]
+  cov_gamma = matrix(0, num_row, num_col)
+  for(i in 1:num_row){
+    for(j in 1:num_col){
+        cov_gamma[i,j] = exp(-dist_mat[i,j] * rho)
+    }
+  }
+  cov_gamma = cov_gamma * sigma_sqaure
+  return(cov_gamma)
+}
+
+# make distance matrix
+CAtemp_data_u1u2= make_chordaldist_u1u2_for_rdist(CAtemp$lat, CAtemp$lon)
+CAtemp_data_dist_mat = rdist(CAtemp_data_u1u2, CAtemp_data_u1u2)
+# dim(dist_mat) # 200 200
 # max(dist_mat)
 # min(dist_mat)
 
-cov_big_gamma = diag(200)
-for(i in 1:200){
-  for(j in 1:200){
-     if(i!=j){
-      cov_big_gamma[i,j] = exp(-dist_mat[i,j] * rho.hat)
-    }
-  }
-}
-cov_spatial = s2.hat*cov_big_gamma
-cov_nugget = diag(200) * tau2.hat
-cov_mat = cov_spatial + cov_nugget
 
+# make covariance matrix
+CAtemp_data_cov_spatial = make_EXPcov_mat_without_nugget(CAtemp_data_dist_mat, rho.hat, s2.hat)
+CAtemp_data_cov_nugget = diag(200) * tau2.hat
+CAtemp_data_cov_mat = CAtemp_data_cov_spatial + CAtemp_data_cov_nugget
+
+
+# gls fit
 b0 = rep(1,200)
 gls.X = as.matrix(data.frame(b0, CAtemp$lon, CAtemp$lat, CAtemp$elevation))
 gls.Y = as.matrix(CAtemp$avgtemp)
-inv_cov_mat = solve(cov_mat)
-gls.beta = solve(t(gls.X) %*% inv_cov_mat %*% gls.X) %*% t(gls.X) %*% inv_cov_mat %*% gls.Y
-(gls.beta)
+CAtemp_data_inv_cov_mat = solve(CAtemp_data_cov_mat)
+gls.beta = solve(t(gls.X) %*% CAtemp_data_inv_cov_mat %*% gls.X) %*% t(gls.X) %*% CAtemp_data_inv_cov_mat %*% gls.Y
+print("gls coefficients")
+cat('(Intercept)    lon       lat    elevation\n',gls.beta,'\n')
+print('----------------')
 
-# (d) Calculate and plot the EBLUP of µ + Z at the locations in CAgrid, plugging in your estimates from (b) and (c). 
+
+
+
+## (d) Calculate and plot the EBLUP of µ + Z at the locations in CAgrid, plugging in your estimates from (b) and (c). 
 # Calculate and plot the (estimated) standard error of Z at each prediction location.
 #lec2.2 page38
 
 # head(CAgrid)
 # dim(CAgrid) #664 1
 
+# make m0(=pred_mu) term(in lec2-2. page38)
 pred_X = as.matrix(data.frame(rep(1,664), CAgrid$lon, CAgrid$lat, CAgrid$elevation))
 pred_mu = pred_X %*% gls.beta
 dim(pred_mu)
 head(pred_mu)
 
 
-#cov
+# make covariance matrix
 #inner
-mean_earth_radius_in_miles = 3958.8 #by google
-pred_chordaldist_x = mean_earth_radius_in_miles * cos(CAgrid$lat) * cos(CAgrid$lon)
-pred_chordaldist_y = mean_earth_radius_in_miles * cos(CAgrid$lat) * sin(CAgrid$lon)
-pred_chordaldist_z = mean_earth_radius_in_miles * sin(CAgrid$lat)
-pred_chordaldist_mat = cbind(pred_chordaldist_x, pred_chordaldist_y, pred_chordaldist_z)
+CAgrid_data_u1u2= make_chordaldist_u1u2_for_rdist(CAgrid$lat, CAgrid$lon)
+pred_inner_dist_mat = rdist(CAgrid_data_u1u2, CAgrid_data_u1u2)
 
-pred_inner_dist_mat = rdist(pred_chordaldist_mat, pred_chordaldist_mat)
-
-pred_cov_big_gamma = diag(664)
-for(i in 1:664){
-  for(j in 1:664){
-     if(i!=j){
-      pred_cov_big_gamma[i,j] = exp(-pred_inner_dist_mat[i,j] * rho.hat)
-    }
-  }
-}
-pred_cov_spatial = s2.hat*pred_cov_big_gamma
-pred_cov_nugget = diag(664) * tau2.hat
-pred_inner_cov_mat = pred_cov_spatial + pred_cov_nugget
+pred_cov_spatial = make_EXPcov_mat_without_nugget(pred_inner_dist_mat, rho.hat, s2.hat)
+# pred_cov_nugget = diag(664) * tau2.hat #no nugget term!
+pred_inner_cov_mat = pred_cov_spatial #+ pred_cov_nugget #no nugget term!
 
 
 #cross
-pred_cross_dist_mat = rdist(chordaldist_mat, pred_chordaldist_mat)
+pred_cross_dist_mat = rdist(CAtemp_data_u1u2, CAgrid_data_u1u2)
 dim(pred_cross_dist_mat) # 200, 664
-pred_cov_cross_gamma = matrix(0, 200, 664)
-for(i in 1:200){
-  for(j in 1:664){
-      pred_cov_cross_gamma[i,j] = exp(-pred_cross_dist_mat[i,j] * rho.hat)
-  }
-}
-pred_cross_cov_mat = pred_cov_cross_gamma * s2.hat
+pred_cross_cov_mat = make_EXPcov_mat_without_nugget(pred_cross_dist_mat, rho.hat, s2.hat)
 
 
-
-#krigging mean
-pred_Y = pred_mu + t(pred_cross_cov_mat) %*% inv_cov_mat %*% (gls.Y - gls.X%*%gls.beta)
+## krigging mean
+pred_Y = pred_mu + t(pred_cross_cov_mat) %*% CAtemp_data_inv_cov_mat %*% (gls.Y - gls.X%*%gls.beta)
 dim(pred_Y)
 CAgrid$pred.temp = pred_Y
 
-# sd of Z
-pred_Y_var_mat = pred_inner_cov_mat - t(pred_cross_cov_mat) %*% inv_cov_mat %*% pred_cross_cov_mat
-dim(pred_Y_var_mat) #664 664
-pred_Y_var = diag(pred_Y_var_mat)
-CAgrid$pred.sd.temp = sqrt(pred_Y_var)
+# sd (I'll skip this. Instead, I'll find mse.)
+pred_Y_var_mat = pred_inner_cov_mat - t(pred_cross_cov_mat) %*% CAtemp_data_inv_cov_mat %*% pred_cross_cov_mat
+# dim(pred_Y_var_mat) #664 664
+# pred_Y_var = diag(pred_Y_var_mat)
+# CAgrid$pred.sd.temp = sqrt(pred_Y_var)
 
-# mse
-cal_b = t(pred_X) - t(gls.X) %*% inv_cov_mat %*% pred_cross_cov_mat
-pred_mse_mat = pred_Y_var_mat + t(cal_b) %*% solve(t(gls.X) %*% inv_cov_mat %*% gls.X) %*% cal_b
+# predicting mse
+cal_b = t(pred_X) - t(gls.X) %*% CAtemp_data_inv_cov_mat %*% pred_cross_cov_mat
+pred_mse_mat = pred_Y_var_mat + t(cal_b) %*% solve(t(gls.X) %*% CAtemp_data_inv_cov_mat %*% gls.X) %*% cal_b
 pred_mse = diag(pred_mse_mat)
 CAgrid$pred.mse.temp = pred_mse
 
 
-# Plotting
-#mean
+## Plotting
+# mean
 range(pred_Y)
 breaks = 30:75
 x11()
@@ -214,27 +239,29 @@ plot.new()
 
 ploteqc(CAgrid, CAgrid$pred.temp, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
-title(main = "Predicted Average Annual Temperatures, 1961-1990, Degrees F")
+title(main = "Predicted Average Annual Temperatures, Degrees F")
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAgrid_predicted_mean.png")
+dev.off()
+
+# # #std
+# # quantile(sqrt(pred_Y_var))
+# # breaks = seq(1.5, 2.15, length.out=40)
+# # x11()
+# # plot.new()
+
+# # ploteqc(CAgrid, CAgrid$pred.sd.temp, breaks, pch = 19)
+# # map("county", region = "california", add = TRUE)
+# # title(main = "Std of Predicted Average Annual Temperatures, 1961-1990, Degrees F")
 
 
-#std
-quantile(pred_Y_std)
-breaks = seq(2, 2.7, length.out=40)
-x11()
-plot.new()
-
-ploteqc(CAgrid, CAgrid$pred.sd.temp, breaks, pch = 19)
-map("county", region = "california", add = TRUE)
-title(main = "Std of Predicted Average Annual Temperatures, 1961-1990, Degrees F")
-
-
-#mse
+# mse
 quantile(pred_mse)
-breaks = seq(7, 7.3, length.out=50)
+breaks = seq(4.6, 5, length.out=50)
 x11()
 plot.new()
 
 ploteqc(CAgrid, CAgrid$pred.mse.temp, breaks, pch = 19)
 map("county", region = "california", add = TRUE)
-title(main = "MSE of Predicted Average Annual Temperatures, 1961-1990, Degrees F")
-
+title(main = "MSE of Predicted Temperatures")
+dev.copy(png,filename="C:/gitProject/SpaTempoDA/HW1/prob2_CAgrid_predicted_MSE.png")
+dev.off()
