fa60cdff0d185e7cf54cf318fa413747fb5d385e
Seokjun Choi <Choi.Seokjun@outlook.com>
2020-04-30 01:00:16 +0900

    HW2 prob3 R codes

--- HW2/HW2sol_problem3.R
+++ HW2/HW2sol_problem3.R
@@ -1,22 +1,37 @@
 # R code created by Choi Seokjun
 # UTF-8 CRLF
 # windows 10 64-bit.
-# R 4.0.0
+# R 3.6.3
 
 
 library(nimble)
 
+library(classInt) # breakpoints for color plotting
 library(mvtnorm)
 library(fields)
 library(sp)
 library(MASS)
 
-source("c:/gitProject/SpaTempoDA/HW2/helper.fns.R")
+
+plot.point.ref <- function(spatialdata, vals) {
+  pal <- c("#0000FF","#FF0000")
+  ints <- classIntervals(vals, n = 2, style = "pretty") # Determine breakpoints
+  # also see style options "quantile" and "fisher"
+  intcols <- findColours(ints, pal) # vector of colors
+  # if pal doesn't have the same length as # classes, findColours will interpolate
+
+  par(mar = rep(0, 4))
+  plot(spatialdata, col = intcols, pch = 19)
+  points(spatialdata, pch = 1)
+  legend("topleft", fill = attr(intcols, "palette"),
+         legend = names(attr(intcols, "table")), bty = "n")
+}
+
 
 
 ## Simulate data
 set.seed(2019311252)
-n = 200
+n = 400
 sim_x = matrix(runif(n*2, min=0, max=1), ncol = 2) # locations
 sim_X = cbind(1, sim_x) # trend surface model
 
@@ -56,12 +71,11 @@ model_string = nimbleCode({
     #Data Model
     for(i in 1:n){
         prob[i] <- exp(W[i]+XB[i])/(1+exp(W[i]+XB[i]))
-        Z[i] ~ dbinom(1,prob[i])
+        Z[i] ~ dbern(prob[i])
     }
     #Constant and Cov Matrix
-    XB[1:n] <- beta1*X[,1] + beta2*X[,2]
-    # covMat[1:n,1:n] <- expcov(dists[1:n,1:n],phi)
-    covMMat[1:n,1:n] <- 1
+    XB[1:n] <- beta0*X[,1] + beta1*X[,2] + beta2*X[,3]
+    covMat[1:n,1:n] <- expcov(dists[1:n,1:n],rho)
     fullCovMat[1:n,1:n] <- sigma2 * covMat[1:n,1:n]
 
     #Process Model
@@ -69,21 +83,23 @@ model_string = nimbleCode({
 
     #Parameter Models
     sigma2 ~ dinvgamma(0.2, 0.2)
-    phi ~ dunif(0,1)
+    rho ~ dunif(0,1)
+    beta0 ~ dnorm(0, sd=sqrt(100))
     beta1 ~ dnorm(0, sd=sqrt(100))
     beta2 ~ dnorm(0, sd=sqrt(100))
 })
 
-niter = 100000
+niter = 200000
 consts = list(n=n, X=sim_X, dists=dist_mat, mn=rep(0,n))
 data = list(Z=sim_Y)
-inits = list(beta1=rnorm(1), beta2=rnorm(1), phi=0.5, sigma2=2, W=rnorm(n))
+inits = list(beta0 = rnorm(1), beta1=rnorm(1), beta2=rnorm(1), rho=0.5, sigma2=2, W=rnorm(n))
 
+pt = proc.time()
 samples = nimbleMCMC(model_string, data=data, inits=inits, constants=consts,
-    monitors=c("beta1","beta2","phi","sigma2","W"),
+    monitors=c("beta1","beta2","rho","sigma2","W"),
     samplesAsCodaMCMC=TRUE, WAIC=FALSE, summary=FALSE,
     niter=niter, nburnin=0, nchains=1)
-
+ptFinal = proc.time()-pt
 
 
 
@@ -109,54 +125,62 @@ hpd <- function(samp,p=0.05){
   return(hpd)
 }
 
-# Exponential Covariance Function
-expCov<-function(distMat,phi){
-  exp(-distMat/phi)
-}
-
-sqeCov<-function(distMat,phi){
-  exp(-0.5*(distMat/phi)^2)
-}
-
-matCov<-function(distMat,phi){
-  (1+(sqrt(5)*(distMat/phi))+((5*distMat^2)/(3*(phi^2))))*exp(-(sqrt(5)*(distMat/phi)))
-}
-
-
-# Matern Cov Function + Acceptance Rate function
-Matern <- function(d, param = c(scale = 1, range = 1, smoothness = 2)) {
-  scale <- param[1]
-  range <- param[2]
-  smoothness <- param[3]
-  if (any(d < 0))
-    stop("distance argument must be nonnegative")
-  d <- d / range
-  d[d == 0] <- 1e-10
-  rootcon<-sqrt(2*smoothness)
-  con <- (2^(smoothness - 1)) * gamma(smoothness)
-  con <- 1 / con
-  return(scale * con * ((rootcon*d)^smoothness) * besselK(rootcon*d, smoothness))
-}
 accRateFunc<-function(x){
   accRate<-(length(unique(x))-1)/(length(x)-1)
   return(accRate)
 }
 
-
 # Summary 
-summaryFunction<-function(mcmcDat,bmseThresh=0.01,time){
-  
+summaryFunction<-function(mcmcDat){
   # Parameters
   summaryMat<-rbind(apply(mcmcDat,2,mean),
                           apply(mcmcDat,2,hpd),
-                          apply(mcmcDat,2,accRateFunc),
-                          bmmat(mcmcDat)[,2],
-                          abs(apply(mcmcDat,2,mean))*bmseThresh,
-                          apply(mcmcDat,2,ess),
-                          apply(mcmcDat,2,ess)/time)
+                          apply(mcmcDat,2,accRateFunc))
   
   rownames(summaryMat)<-c("Mean","95%CI-Low","95%CI-High",
-                                "Accept","BMSE",paste(bmseThresh,"x mean"),
-                                "ESS","ESS/sec")
+                                "Accept")
   return(summaryMat)
 }
+
+plotFunction = function(mcmcDat, trueParamVal, paramName){
+  dev.new(width=15, height=7.5, unit="cm")
+
+  par(mfrow=c(1,2))
+  MCsample_mean = mean(mcmcDat)
+  MCsample_hpd = hpd(mcmcDat)
+
+  plot.ts(mcmcDat)
+  abline(h=trueParamVal, col="red", lwd=2)
+  abline(h=MCsample_mean, col="blue", lwd=2)
+  title(paste("traceplot of", paramName))
+  text(0,trueParamVal,label="TRUE", col="red", pos=3)
+  text(0,MCsample_mean,label="MCMC", col="blue", pos=3)
+
+
+  plot(density(mcmcDat), main=paste("emp. density of", paramName))
+  abline(v=trueParamVal, col="red", lwd=2)
+  abline(v=MCsample_mean, col="blue", lwd=2)
+  text(trueParamVal,0.001,label="TRUE",col="red",pos=3)
+  text(MCsample_mean,0.001,label="MCMC",col="blue",pos=3)
+  lines(MCsample_hpd,c(0.001,0.001), col="blue",lwd=3)
+  text(MCsample_hpd[1],0.001,label="95%HPD",col="blue",pos=3)
+
+  plot_filename=paste("C:/gitProject/SpaTempoDA/HW2/prob3_",paramName,".png",sep="")
+  dev.copy(png,filename=plot_filename)
+  dev.off()
+}
+
+
+
+SummaryMat = list()
+SummaryMat[[1]] = summaryFunction(samples[,c("beta1","beta2","sigma2","rho")])
+SummaryMat[[2]] = summaryFunction(samples[,1:n])
+# save(SummaryMat,samples,ptFinal,file="C:/gitProject/SpaTempoDA/HW2/prob2_MCMCresult.RData")
+print(SummaryMat[[1]])
+
+# beta = c(0, 1, 1); sigma2 = 0.1; rho = 0.1 # true parameters
+plotFunction(samples[10000:200000,"sigma2"], 0.1, "sigma2")
+plotFunction(samples[10000:200000,"beta1"], 1, "beta1")
+plotFunction(samples[10000:200000,"beta2"], 1, "beta2")
+plotFunction(samples[10000:200000,"rho"], 0.1, "rho")
+
